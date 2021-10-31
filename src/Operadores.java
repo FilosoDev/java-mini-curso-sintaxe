360f8c0584cc4716ecd4eb9e0f7f64b19c02233e
Arthur <arthurferreira@usp.br>
2021-10-31 10:59:22 -0300

    Aula Operadores parte 1.

---
+++ src/Operadores.java
@@ -0,0 +1,53 @@
+import java.lang.Math;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		
+		int numA = 5;
+		int numB = 2;
+		
+		// Soma
+		int total = numA + numB;
+		System.out.println(total);
+		
+		// Subtração
+		total = numA - numB;
+		System.out.println(total);
+		
+		// Multiplicação
+		total = numA * numB;
+		System.out.println(total);
+		
+		// Divisão
+		total = numA / numB;
+		System.out.println(total);
+		double numC, numD, totalDouble;
+		numC = 5;
+		numD = 2;
+		totalDouble = numC / numD;
+		System.out.println(totalDouble);
+		
+		// Resto da divisão (MOD)
+		total = numA % numB;
+		System.out.println(total);
+		
+		// Potenciação
+		double numE, numF, potencia;
+		numE = 6;
+		numF = 3;
+		potencia = Math.pow(numE, numF);
+		System.out.println(potencia);
+		
+		// Radiciação (Raiz quadrada)
+		double raiz;
+		raiz = Math.sqrt(100);
+		System.out.println(raiz);
+		
+		// Raiz cúbica
+		raiz = Math.cbrt(27);
+		System.out.println(raiz);
+
+	}
+
+}
