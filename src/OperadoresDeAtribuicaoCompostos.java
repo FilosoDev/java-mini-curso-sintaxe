1e066c9933c4ae0aa3cea198a46241f40f778e62
Arthur <arthurferreira@usp.br>
2021-10-31 20:42:36 -0300

    Aula Operadores final.

---
+++ src/OperadoresDeAtribuicaoCompostos.java
@@ -0,0 +1,21 @@
+
+public class OperadoresDeAtribuicaoCompostos {
+
+	public static void main(String[] args) {
+		
+		int numero = 10;
+		
+		numero += 3; // equivalente numero = numero + 3;
+		System.out.println(numero);
+
+		numero -= 2; // equivalente a numero = numero -2;
+		System.out.println(numero);
+		
+		numero *= 4; // equivalente a numero = numero * 4;
+		System.out.println(numero);
+		
+		numero /= 2; // equivalente a numero = numero / 2;
+		System.out.println(numero);
+	}
+
+}
