756b7174a01fe1ce57e7758a179c7e3d06b39f86
Arthur <arthurferreira@usp.br>
2021-11-01 13:56:11 -0300

    Aula Operadores relacionais.

---
+++ src/OperadoresRelacionais.java
@@ -0,0 +1,25 @@
+
+public class OperadoresRelacionais {
+
+	public static void main(String[] args) {
+		
+		// == igualdade
+		System.out.println(1 == 2);
+
+		// != diferença
+		System.out.println(1 != 2);
+		
+		// > maior
+		System.out.println(1 > 0);
+		
+		// < menor
+		System.out.println(1 < 0);
+		
+		// >= maior ou igual
+		System.out.println(1 >= 1);
+		
+		// <= menor ou igual
+		System.out.println(1 <= 2);
+	}
+
+}
