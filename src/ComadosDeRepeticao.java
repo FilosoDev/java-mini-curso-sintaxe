fa63295dd8cc6c85d21e9a2121736d1e95a3c452
Arthur <arthurferreira@usp.br>
2021-11-01 20:15:07 -0300

    Aula Switch.

--- src/ComadosDeRepeticao.java
+++ src/ComadosDeRepeticao.java
@@ -3,24 +3,24 @@ public class ComadosDeRepeticao {
 
 	public static void main(String[] args) {
 
-		// int contador = 1;
+		int contador = 1;
 
 		// while
-		// while(contador <= 10) {
-		// System.out.println(contador);
-		// contador++;
-		// }
+		while(contador <= 10) {
+			System.out.println(contador);
+			contador++;
+		}
 
 		// do while
-		// int contadorDois = 1;
-		// do {
-		// System.out.println(contadorDois);
-		// contadorDois++;
-		// }while(contadorDois <= 10);
+		int contadorDois = 1;
+		do {
+			System.out.println(contadorDois);
+			contadorDois++;
+		}while(contadorDois <= 10);
 
-		//for (int x = 1; x <= 10; x++) {
-		//	System.out.println(x);
-		//}
+		for (int x = 1; x <= 10; x++) {
+			System.out.println(x);
+		}
 		
 		String[] frutas = {"Maçã", "Melão", "Banana", "Abacate"};
 		for(int x = 0; x < frutas.length; x++) {
---
+++ src/ComandoDeSelecao.java
@@ -0,0 +1,22 @@
+
+public class ComandoDeSelecao {
+
+	public static void main(String[] args) {
+		
+		int escolha = 99;
+		switch(escolha) {
+		case 1:
+			System.out.println("Número 1");
+			break;
+		case 2:
+			System.out.println("Número 2");
+			break;
+		case 3:
+			System.out.println("Número 3");
+			break;
+		default:
+			System.out.println("Outro número");
+		}
+	}
+
+}
