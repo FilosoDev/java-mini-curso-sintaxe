53b0d1661640bc543507786d422d23e80c05e5ee
Arthur <arthurferreira@usp.br>
2021-10-31 21:17:54 -0300

    Aula Condicionais.

---
+++ src/Condicionais.java
@@ -0,0 +1,49 @@
+
+public class Condicionais {
+
+	public static void main(String[] args) {
+		
+		double notaA, notaB, media;
+		notaA = 5;
+		notaB = 5;
+		media = (notaA + notaB) / 2;
+		
+		
+		if(media >= 7) {
+			System.out.println(media + " Aprovado");
+		}else {
+			System.out.println(media + " Reprovado");
+		}
+		
+		
+		
+		if(media >= 7) {
+			System.out.println(media + " Aprovado");
+		}else if(media >= 5 && media < 7) {
+			System.out.println(media + " Em recuperação");
+		}else {
+			System.out.println(media + " Reprovado");
+		}
+		
+		
+		// Operadores lógicos: && (e) - || (ou)
+		int faltas = 10;
+		if(media >= 7 && faltas <= 10) {
+			System.out.println("Aprovado " + faltas + " faltas - nota: " + media);
+		}else if(media < 5 || faltas > 10){
+			System.out.println("Reprovado " + faltas + " faltas - nota: " + media);
+		}else if(media >= 5 && media < 7 && faltas <= 10){
+			System.out.println("Recuperação " + faltas + " faltas - nota: " + media);
+		}else {
+			System.out.println("Reprovado " + faltas + " faltas - nota: " + media);
+		}
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
